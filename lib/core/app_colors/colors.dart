a07a9cc115cb2018e2515eb0433e7e74ff621701
Jalolxon <85275641+aaqaaaaaaaa@users.noreply.github.com>
2023-03-22 17:29:24 +0500

    fixed

--- lib/core/app_colors/colors.dart
+++ lib/core/app_colors/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static Color primaryColor = const Color(0xFFE15381);
+  static Color primaryColor = const Color(0xFF114BE5);
   static Color whiteColor = const Color(0xFFffffff);
   static Color textColor = const Color(0xFF1C1C1C);
 }
